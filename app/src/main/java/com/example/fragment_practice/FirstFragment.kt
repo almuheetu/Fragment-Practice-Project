c4306a8dc7d4997f4a1f78cb5aee06dc0a425abc
Almuheetu shihab <almuheetu@gmail.com>
2024-08-19 14:34:25 +0600

    all fragment lifecycle add and show toast

--- app/src/main/java/com/example/fragment_practice/FirstFragment.kt
+++ app/src/main/java/com/example/fragment_practice/FirstFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.fragment_practice.databinding.FirstFragmentXmlBinding
 
@@ -12,6 +13,7 @@ class FirstFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Toast.makeText(requireContext(), "onCreate Called", Toast.LENGTH_SHORT).show()
     }
 
     override fun onCreateView(
@@ -20,12 +22,86 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FirstFragmentXmlBinding.inflate(inflater, container, false)
+        Toast.makeText(requireContext(), "onCreateView Called", Toast.LENGTH_SHORT).show()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Toast.makeText(requireContext(), "onViewCreated Called", Toast.LENGTH_SHORT).show()
+    }
 
-
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(requireContext(), "onStart Called", Toast.LENGTH_SHORT).show()
+    }
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(requireContext(), "onResume Called", Toast.LENGTH_SHORT).show()
     }
 }
+
+
+
+
+
+
+//
+//class FirstFragment : Fragment() {
+//
+//    private lateinit var binding: FirstFragmentXmlBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        Toast.makeText(requireContext(), "onCreate Called", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        binding = FirstFragmentXmlBinding.inflate(inflater, container, false)
+//        Toast.makeText(requireContext(), "onCreateView Called", Toast.LENGTH_SHORT).show()
+//        return binding.root
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        Toast.makeText(requireContext(), "onViewCreated Called", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    override fun onStart() {
+//        super.onStart()
+//        Toast.makeText(requireContext(), "onStart Called", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        Toast.makeText(requireContext(), "onResume Called", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    override fun onPause() {
+//        super.onPause()
+//        Toast.makeText(requireContext(), "onPause Called", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//        Toast.makeText(requireContext(), "onStop Called", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        Toast.makeText(requireContext(), "onDestroyView Called", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        Toast.makeText(requireContext(), "onDestroy Called", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    override fun onDetach() {
+//        super.onDetach()
+//        Toast.makeText(requireContext(), "onDetach Called", Toast.LENGTH_SHORT).show()
+//    }
+//}
